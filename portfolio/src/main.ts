dfc751f4d254754f5984dd9cbf74e8695a069c82
aron <aron@beeldr.nl>
2026-04-23 16:59:24 +0200

    Add project start also added motion

--- portfolio/src/main.ts
+++ portfolio/src/main.ts
@@ -6,9 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import { MotionPlugin } from 'motion-v'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(MotionPlugin)
 
 app.mount('#app')
--- portfolio/src/views/home.vue
+++ portfolio/src/views/home.vue
@@ -1,3 +1,11 @@
 <template>
     hello world
+
+    <button @click="clicked = !clicked" v-motion :initial="{ scale: 0 }" :animate="{ scale: 1 }">Click me</button>
 </template>
+
+<script setup>
+    import { ref } from 'vue';
+
+    const clicked = ref(false);
+</script>
